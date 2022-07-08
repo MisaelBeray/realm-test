d6dd1e5cd7bf9bc37c31c6816a993249df9b283f
Misael Beray <misaelberay@gmail.com>
2022-07-08 11:08:37 -0300

    feat(draw): added dynamic input text

--- src/app/pages/DrawPointModal/index.tsx
+++ src/app/pages/DrawPointModal/index.tsx
@@ -5,12 +5,14 @@ import {
   PanResponder,
   Animated,
   ImageBackground,
+  Dimensions,
 } from 'react-native';
 import Svg, {Line} from 'react-native-svg';
 import {launchImageLibrary} from 'react-native-image-picker';
 import {FAB} from 'react-native-paper';
 import ViewShot from 'react-native-view-shot';
 import CameraRoll from '@react-native-community/cameraroll';
+import {DragTextEditor} from 'react-native-drag-text-editor';
 const DrawPointModal: FC = () => {
   const styles = StyleSheet.create({
     MainContainer: {
@@ -117,6 +119,26 @@ const DrawPointModal: FC = () => {
 
   const [imagePosition, setImagePosition] = useState<Animated.ValueXY>(pan);
 
+  const WINDOW = Dimensions.get('window');
+
+  const [addText, setAddText] = useState<TextEditor[]>([]);
+
+  interface TextEditor {
+    minWidth: number;
+    minHeight: number;
+    w: number;
+    h: number;
+    x: number;
+    y: number;
+    FontColor: string;
+    LineHeight: number;
+    TextAlign: string;
+    LetterSpacing: number;
+    FontSize: number;
+    isDraggable: boolean;
+    isResizable: boolean;
+    value: string;
+  }
   return (
     <View style={styles.MainContainer}>
       <ViewShot
@@ -163,6 +185,27 @@ const DrawPointModal: FC = () => {
             })}
           </Svg>
         </Animated.View>
+        {addText.map((text, index) => {
+          return (
+            <DragTextEditor
+              key={index}
+              minWidth={text.minWidth}
+              minHeight={text.minHeight}
+              w={text.w}
+              h={text.h}
+              x={text.x}
+              y={text.y}
+              FontColor={text.FontColor}
+              LineHeight={text.LineHeight}
+              TextAlign={text.TextAlign}
+              LetterSpacing={text.LetterSpacing}
+              FontSize={text.FontSize}
+              isDraggable={text.isDraggable}
+              isResizable={text.isResizable}
+              text=""
+            />
+          );
+        })}
       </ViewShot>
 
       <FAB.Group
@@ -178,9 +221,33 @@ const DrawPointModal: FC = () => {
               setScale(1);
               setImagePosition(new Animated.ValueXY());
 
-              viewShotRef?.current?.capture?.().then(uri => {
+              /* viewShotRef?.current?.capture?.().then(uri => {
                 CameraRoll.save(uri);
-              });
+              }); */
+            },
+          },
+          {
+            icon: 'tooltip-text-outline',
+            small: false,
+            onPress: () => {
+              const text: TextEditor = {
+                minWidth: 40,
+                minHeight: 40,
+                w: 80,
+                h: 80,
+                x: WINDOW.width / 4,
+                y: WINDOW.height / 3,
+                FontColor: 'red',
+                LineHeight: 15,
+                TextAlign: 'left',
+                LetterSpacing: 0,
+                FontSize: 22,
+                isDraggable: true,
+                isResizable: true,
+                value: '',
+              };
+
+              setAddText(arr => [...arr, text]);
             },
           },
           {
